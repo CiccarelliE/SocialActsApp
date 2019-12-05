23f70e908ae6badb3c1cc3b0d237eb5d5ce3c18a
Cesar Alcantara <cja5391@psu.edu>
2019-12-05 15:44:30 -0500

    added cors, axios, loop through api data

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    values: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/values")
+      .then(response => {
+        console.log(response);
+        this.setState({
+          values: response.data
+        });
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li key={value.id}>{value.name}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
